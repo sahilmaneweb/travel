eac2a8a9d05166fcff1c467b8f8b953e0f859f45
Sahil Mane <142393824+sahilmaneweb@users.noreply.github.com>
2024-06-10 16:16:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const block = document.getElementsByClassName("categories");
+function extend()
+{
+    var l = document.getElementById("cat").style;
+    if(l.height === "230px"){
+        l.height="100%";
+    }
+    else{
+        l.height="230px";
+    }
+    console.log("hi");
+}
